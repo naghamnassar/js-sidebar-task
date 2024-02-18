72bb419f4e1fe1758f513760be6d247948a4f7f9
naghamnassar <147164962+naghamnassar@users.noreply.github.com>
2024-02-18 19:46:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('toggleButton');
+    const sidebar = document.getElementById('sidebar');
+  
+    toggleButton.addEventListener('click', function () {
+      sidebar.classList.toggle('show-sidebar');
+    });
+  });
+  
+
+  
+  
+  
+  
+  
